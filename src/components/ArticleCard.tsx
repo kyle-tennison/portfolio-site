7f6d035b289ad117a2dd2c5155968d26e91617fe
Kyle Tennison <kyletennison05@gmail.com>
2025-05-15 00:35:39 -0700

    feat: articles index

---
+++ src/components/ArticleCard.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/ArticleCard.css";
+
+interface ArticleCardProps {
+  title: string;
+  date: string;
+  description: string;
+  link: string;
+}
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ title, date, description, link }) => {
+  return (
+    <a href={link} className="article-card">
+      <div className="article-card-content">
+        <p className="article-card-title">
+          <strong>{title}</strong> &bull; <em>{date}</em>
+        </p>
+        <p className="article-card-description">{description}</p>
+      </div>
+      <i className="bi bi-box-arrow-in-up-right"></i>
+    </a>
+  );
+};
+
+export default ArticleCard;
--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,4 +1,37 @@
-import React from 'react';
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+import "../styles/Articles.css";
+import ArticleCard from "../components/ArticleCard";
+
 export default function Articles() {
-  return <h1>Articles</h1>;
+  return (
+    <div id="articles-page">
+      <Header />
+
+      <div className="title-text">
+        <h1>Articles</h1>
+        <p>
+          This page serves as a spot to self-publish my miscellaneous articles,
+          blogs, papers, etc.
+        </p>
+      </div>
+
+      <div className="card-container">
+        <ArticleCard
+          title={"Franklin"}
+          date={"February 2024"}
+          description={
+            "An overview of how my self-balancing robot, Franklin, was designed, fabricated, and trained. I go into depth about the Onshape model, my KiCAD PCB, and more."
+          }
+          link={"articles/franklin"}
+        />
+
+
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
