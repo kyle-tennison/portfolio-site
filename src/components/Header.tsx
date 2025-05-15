e020f7b9320e052273dff3f57731ad109a60c026
Kyle Tennison <kyletennison05@gmail.com>
2025-05-14 21:33:16 -0700

    feat: add header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import "../styles/Header.css"
 export default function Header() {
     return <div id="header">
         <div className="left">
-            <a className="logo">K.T.</a>
+            <a className="logo" href="/">K.T.</a>
         </div>
         <div className="right">
             <nav>
-                <a className="averia-serif-libre-light">Contact</a>
-                <a className="averia-serif-libre-light">Resume</a>
-                <a className="averia-serif-libre-light">Projects</a>
-                <a className="averia-serif-libre-light">Articles</a>
+                <a href="/contact" className="averia-serif-libre-light">Contact</a>
+                <a href="/resume" className="averia-serif-libre-light">Resume</a>
+                <a href="/projects" className="averia-serif-libre-light">Projects</a>
+                <a href="/articles" className="averia-serif-libre-light">Articles</a>
             </nav>
         </div>
     </div>
