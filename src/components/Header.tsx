99445993dbf5fae06534cc8a836f391b5e74d52c
Kyle Tennison <kyletennison05@gmail.com>
2025-05-15 21:23:09 -0700

    feat: mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,70 @@
+import React, { useRef } from "react";
+import { slide as Menu } from "react-burger-menu";
 import "../styles/Header.css";
 
-export default function Header() {
+export const Header: React.FC = () => {
+  const menuRef = useRef<any>(null);
+
+
   return (
-    <div id="header">
-      <div className="left">
-        <a className="logo" href="/">
-          K.T.
+    <header id="header">
+      <div className="header-bar">
+        <div className="left">
+          <a className="logo" href="/">
+            K.T.
+          </a>
+        </div>
+
+        <div className="right">
+          <nav className="desktop-nav">
+            <a href="/articles/contact" className="averia-serif-libre-light">
+              Contact
+            </a>
+            <a
+              href="/resume.pdf"
+              target="_blank"
+              referrerPolicy="no-referrer"
+              className="averia-serif-libre-light"
+            >
+              Resume
+            </a>
+            <a href="/projects" className="averia-serif-libre-light">
+              Projects
+            </a>
+            <a href="/articles" className="averia-serif-libre-light">
+              Articles
+            </a>
+          </nav>
+        </div>
+      </div>
+
+      <Menu
+        right
+        customBurgerIcon={<i className="bi bi-list"></i>}
+        noOverlay
+        className="mobile-menu"
+        ref={menuRef}
+      >
+        <a href="/articles/contact" className="menu-item">
+          Contact
         </a>
-      </div>
-      <div className="right">
-        <nav>
-          <a href="/articles/contact" className="averia-serif-libre-light">
-            Contact
-          </a>
-          <a
-            href="/resume.pdf"
-            target="_blank"
-            referrerPolicy="no-referrer"
-            className="averia-serif-libre-light"
-          >
-            Resume
-          </a>
-          <a href="/projects" className="averia-serif-libre-light">
-            Projects
-          </a>
-          <a href="/articles" className="averia-serif-libre-light">
-            Articles
-          </a>
-        </nav>
-      </div>
-    </div>
+        <a
+          href="/resume.pdf"
+          target="_blank"
+          referrerPolicy="no-referrer"
+          className="menu-item"
+        >
+          Resume
+        </a>
+        <a href="/projects" className="menu-item">
+          Projects
+        </a>
+        <a href="/articles" className="menu-item">
+          Articles
+        </a>
+      </Menu>
+    </header>
   );
-}
+};
+
+export default Header;
