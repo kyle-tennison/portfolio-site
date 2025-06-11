ccfaa1d62d68cbc4576e72526d9415936f70f292
kyle-tennison <kyletennison05@gmail.com>
2025-06-11 12:12:57 -0700

    fix: franklin article read time

--- src/articles/Franklin/Franklin.tsx
+++ src/articles/Franklin/Franklin.tsx
@@ -22,7 +22,7 @@ export default function FranklinArticle() {
         title="Franklin"
         date="February 2024"
         description="All about Franklin: my 3D printed self-balancing robot"
-        readTime="0 minutes"
+        readTime="5 minute"
         titleImage="https://polybrain.b-cdn.net/contribute-art.png"
       >
         <h1>Background</h1>
